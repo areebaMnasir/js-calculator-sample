2056cd564b7f97acfe419a19280669ed5fa7f3e3
areebaMnasir <142738658+areebaMnasir@users.noreply.github.com>
2023-11-01 15:49:52 +0500

    JS -assignment-sample
    file:///C:/Users/PC/3D%20Objects/Desktop/calculator/index.html

---
+++ app.js
@@ -0,0 +1,23 @@
+function Add (){
+    var value1 = + prompt("Enter value number one")
+    var value2 = + prompt("Enter value number two")
+     alert(value1 + value2)
+}
+function Sub (){
+    var value1 = + prompt("Enter value number one")
+    var value2 = + prompt("Enter value number one")
+    alert(value1 - value2)
+}
+function Mult (){
+    var value1 = + prompt("Enter value number one")
+    var value2 = + prompt("Enter value number one")
+    alert(value1 * value2)
+}
+function Div (){
+    var value1 = + prompt("Enter value number one")
+    var value2 = + prompt("Enter value number one")
+    alert(value1 / value2)
+}
+
+
+
